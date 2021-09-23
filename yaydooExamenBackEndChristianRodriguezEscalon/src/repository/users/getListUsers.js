ebcd2c12b68e29d672eaae758aee2f2e6261f0ef
Christian Alejandro Rodriguez Escalon <alejandroazteca@hotmail.com>
2021-09-23 10:19:20 -0500

    Update getListUsers.js

--- yaydooExamenBackEndChristianRodriguezEscalon/src/repository/users/getListUsers.js
+++ yaydooExamenBackEndChristianRodriguezEscalon/src/repository/users/getListUsers.js
@@ -9,7 +9,7 @@ const exec = require('../getDatosMsSQL');
 
 //Get List Users
 const getListUsers = async () => {
-  let query = `SELECT name, email, address, telephone, FORMAT (dateofbirth, 'dd/MM/yyyy') dateofbirth, '**********' password, A.id
+  let query = `SELECT name, email, address, telephone, FORMAT (dateofbirth, 'dd/MM/yyyy') dateofbirth, '**********' password, A.id, DATEDIFF(YEAR,dateofbirth,GETDATE()) age
                 FROM Users A
                 INNER JOIN DetailsUser B ON A.ID = B.IDUser`;
 
